f73b82e2cf01692cc66bdad64712c0ab7958f7f5
Aleksey <lekha.bolt2004@gmail.com>
2024-11-24 16:33:38 +0300

    десериализатор

--- src/main/java/ru/t1/java/demo/kafka/MessageDeserializer.java
+++ src/main/java/ru/t1/java/demo/kafka/MessageDeserializer.java
@@ -1,16 +1,17 @@
 package ru.t1.java.demo.kafka;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.header.Headers;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.stereotype.Component;
-
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class MessageDeserializer<T> extends JsonDeserializer<T> {
 
     private static String getMessage(byte[] data) {
@@ -22,20 +23,20 @@ public class MessageDeserializer<T> extends JsonDeserializer<T> {
         try {
             return super.deserialize(topic, headers, data);
         } catch (Exception e) {
-            log.warn("Произошла ошибка во время десериализации сообщения {}", new String(data, StandardCharsets.UTF_8), e);
+            log.error("Произошла ошибка в топике {} при десериализации сообщения {}: {}",
+                    topic, getMessage(data), e.getMessage(), e);
             return null;
         }
     }
 
     @Override
     public T deserialize(String topic, byte[] data) {
-        try {
+        try{
             return super.deserialize(topic, data);
         } catch (Exception e) {
-            log.warn("Произошла ошибка во время десериализации сообщения {}",
-                    new String(data, StandardCharsets.UTF_8), e);
+            log.error("Произошла ошибка в топике {} при десериализации сообщения {}: {}",
+            topic, getMessage(data), e.getMessage(), e);
             return null;
         }
     }
-
 }
